9c72483eac998f53a39374adc32aec39dc2f71c6
Rajnil Saha <rajnilhere@gmail.com>
2024-10-27 01:14:41 +0530

    Create happy.java
    Programme to determine number is happy or not in Java

---
+++ Java/happy.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int num = n;
+        while(n>9){
+            int x = 0;
+            while(n!=0){
+                int a = n%10;
+                n/=10;
+                x += (a*a);
+            }
+            n=x;
+        }
+        if(n==1 || n==7){
+            return true;
+        }
+        return false;
+    }
+}
